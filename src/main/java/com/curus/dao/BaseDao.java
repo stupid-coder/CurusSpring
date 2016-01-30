efe9883f7353ff1410ce2a91cf74b81c306f4a5d
stupid-coder <stupid_coder@163.com>
2016-01-30 23:11:40 +0800

    update BaseDao bug

--- src/main/java/com/curus/dao/BaseDao.java
+++ src/main/java/com/curus/dao/BaseDao.java
@@ -95,7 +95,7 @@ public class BaseDao<T> extends JdbcDaoSupport {
         List<Object> args = new ArrayList<Object>();
         String whereSql = buildWhereSql(where,args);
         RowMapper<T> rowMapper = BeanPropertyRowMapper.newInstance(entityClass);
-        List<T> rs = getJdbcTemplate().query(String.format("SELECT * FROM %s WHERE %s", entityClass.getSimpleName(), whereSql), rowMapper, args.toArray());
+        List<T> rs = getJdbcTemplate().query(String.format("SELECT * FROM %s %s", entityClass.getSimpleName(), whereSql), rowMapper, args.toArray());
         if (rs.isEmpty()) return null;
         else return rs;
     }
