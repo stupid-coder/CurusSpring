1bad25ba74b279261d9d7ec93c9ce0bbfa7024d8
stupid-coder <stupid_coder@163.com>
2016-03-17 17:32:35 +0800

    Change To Redis

---
+++ src/main/java/com/curus/io/RedisCache.java
@@ -0,0 +1,73 @@
+package com.curus.io;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by stupid-coder on 9/3/16.
+ */
+public class RedisCache {
+
+    static private Log logger = LogFactory.getLog(RedisCache.class);
+
+    static private String ADDR = "123.57.227.101";
+    static private int PORT = 6379;
+    static private String PREFIX = "CURUS";
+    private static int expire = 7*24*3600;
+    private static int MAX_TOTAL = 8;
+    private static int MAX_IDLE = 8;
+
+    private static int MAX_WAIT = 10000;
+    private static int TIMEOUT = 10000;
+
+    private static boolean TEST_ON_BORROW = true;
+    private static JedisPool jedisPool = null;
+
+    static {
+        try {
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(MAX_TOTAL);
+            config.setMaxIdle(MAX_IDLE);
+            config.setMaxWaitMillis(MAX_WAIT);
+            config.setTestOnBorrow(TEST_ON_BORROW);
+            jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT);
+        } catch ( Exception e ) {
+            logger.warn("Failure to Static Initilization the Jedis");
+        }
+    }
+
+    static String getKey( String key ) {
+        return String.format("%s_%s",PREFIX,key);
+    }
+
+    static public void Add(String key, String value) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.set(getKey(key),value);
+        jedis.expire(getKey(key),expire);
+        jedisPool.returnResource(jedis);
+    }
+
+    static public String Get(String key) {
+        Jedis jedis = jedisPool.getResource();
+        String value = jedis.get(getKey(key));
+        jedisPool.returnResource(jedis);
+        return value;
+    }
+
+    static public void Delete(String key) {
+        Jedis jedis = jedisPool.getResource();
+        jedis.del(getKey(key));
+    }
+
+    static public String DeleteAndGet(String key) {
+        Jedis jedis = jedisPool.getResource();
+        String k = getKey(key);
+        String v = jedis.get(k);
+        if (v != null) jedis.del(k);
+        return v;
+    }
+
+}
--- src/main/java/com/curus/utils/CacheUtils.java
+++ src/main/java/com/curus/utils/CacheUtils.java
@@ -1,6 +1,7 @@
 package com.curus.utils;
 
 import com.curus.io.Cache;
+import com.curus.io.RedisCache;
 import com.curus.utils.constant.CateConst;
 import com.curus.utils.validate.CateValidate;
 import org.apache.commons.lang.RandomStringUtils;
@@ -39,20 +40,20 @@ public class CacheUtils {
         String ckey = getCodeCacheKey(cate, token, phone);
         if (ckey != null) {
             System.out.println(String.format("Put Code 2 Cache: %s-%s",ckey,code));
-            Cache.Add(ckey, code);
+            RedisCache.Add(ckey, code);
         }
     }
 
     public static String getCode4Cache(String cate, String token, String phone) {
         String ckey = getCodeCacheKey(cate, token, phone);
-        if (ckey != null) return Cache.Get(ckey);
+        if (ckey != null) return RedisCache.Get(ckey);
         else return null;
     }
 
     public static void deleteCode4Cache(String cate, String token,String phone) {
         String ckey = getCodeCacheKey(cate, token, phone);
         if ( ckey != null)
-            Cache.Delete(ckey);
+            RedisCache.Delete(ckey);
     }
 
     public static String getToken() {
@@ -60,19 +61,19 @@ public class CacheUtils {
     }
 
     public static void putObject2Cache(String key, Object o) {
-        Cache.Add(key, SerializeUtils.serialize2string(o));
+        RedisCache.Add(key, SerializeUtils.serialize2string(o));
     }
     public static String putObject2Cache(Object o) { String token = getToken(); putObject2Cache(token,o); return token;}
 
     public static Object getObject4Cache(String key) {
-        return SerializeUtils.unserialize(Cache.Get(key));
+        return SerializeUtils.unserialize(RedisCache.Get(key));
     }
 
     public static void deleteObject4Cache(String key) {
-        Cache.Delete(key);
+        RedisCache.Delete(key);
     }
 
     public static Object deleteObjectAndGet4Cache(String key) {
-        return SerializeUtils.unserialize(Cache.DeleteAndGet(key));
+        return SerializeUtils.unserialize(RedisCache.DeleteAndGet(key));
     }
 }
